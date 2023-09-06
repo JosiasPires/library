89f1ab442df05f6570b7c0c1f0b2225ce256a967
JosiasPires <akiles26@protonmail.com>
2023-09-06 00:05:30 -0300

    Display books

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-const myLibrary = [];
+const body = document.body;
+
+const myLibrary = [
+    {
+        title: 'The Hobbit',
+        author: 'A.J. William',
+        pages: 318,
+        read: true
+    },
+    {
+        title: 'The Hobbit',
+        author: 'A.J. William',
+        pages: 318,
+        read: true
+    },
+    {
+        title: 'The Hobbit',
+        author: 'A.J. William',
+        pages: 318,
+        read: true
+    }
+];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -15,3 +36,25 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function displayLibrary() {
+    for (let book of myLibrary) {
+        let card = document.createElement("div");
+        let title = document.createElement("h3");
+        let author = document.createElement("p");
+        let pages = document.createElement("p");
+        let read = document.createElement("p");
+        title.textContent = book.title;
+        author.textContent = `By ${book.author}`;
+        pages.textContent = `Pages: ${book.pages}`;
+        read.textContent = book.read ? "Status: Read" : "Status: Not read yet";
+        card.classList.add("card");
+        title.classList.add("title");
+        card.append(title);
+        card.append(author);
+        card.append(pages);
+        card.append(read);
+        body.appendChild(card);
+    }
+}
+
+displayLibrary();
